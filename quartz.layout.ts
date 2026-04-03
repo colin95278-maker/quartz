7b436545b973adb4d510a680f6f01321f4d64a68
colin2099 <ccclll502@gmail.com>
2026-04-03 00:20:02 -0600

    极简 Medium 风格改造：纯白背景、大字号、宽间距、简化布局

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -36,9 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
